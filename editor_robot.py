117d1b58932b51f5a91fd007dfef0ee1332dd1c8
Dkner <appljuic@hotmail.com>
2017-05-11 11:01:52 +0800

    more edit

--- editor_robot.py
+++ editor_robot.py
@@ -1,55 +1,158 @@
 import os
+from random import choice
 import itchat
 from itchat.content import *
 
 
 class EditorRobot(object):
+    division_group_map = {
+        '1023': '名片全能王 - 机械机电自动化交流群',
+        '1024': '名片全能王 - 金融行业交流群',
+        '1025': '名片全能王 - IT互联网行业交流群',
+        '1026': '名片全能王 - 房产建筑行业交流群',
+        '1027': '名片全能王 - 快消零售交流群',
+        '1028': '名片全能王 - 广告媒体交流群',
+        '1029': '名片全能王 - 教育行业交流群',
+        '1030': '名片全能王 - 医疗行业交流群',
+        '1031': '名片全能王 - 电子电器行业交流群',
+        '1032': '名片全能王 - 交通运输物流交流群',
+        '1033': '名片全能王 - 化工行业交流群',
+        '1034': '名片全能王 - 冶炼五金交流群',
+        '1035': '名片全能王 - 能源资源交流群',
+    }
+    editor_pic = ['chenchen.png', 'miaomiao.png', 'nana.png', 'xiaofang.png', 'xiaoyi.png', 'xiaozhi.png']
+    center_group = ['名片全能王3群-商务资源对接', '名片全能王19群-商务资源对接', '名片全能王20群-商务资源对接']
 
     def __init__(self, id, robot):
         self.id = id
         self.robot = robot
 
+
+class FenqunRobot(EditorRobot):
+
+    def __init__(self, id, robot):
+        EditorRobot.__init__(self, id, robot)
+
         @robot.msg_register([TEXT])
         def auto_reply(msg):
-            auto_reply = '''【进群前，请仔细阅读名片全能王社群 - 群规】
-1. 本群为实名商务社群，入群后先修改自己的群昵称：地域+行业+职位+姓名。
-2. 请将自己的电子名片发至群内，方便大家互相收藏，更方便快捷的完成合作需求对接。
-3. 请勿大量私加群友进行推销等骚扰行为，一经举报则将清除社群，并列出名片全能王社群黑名单。
-4. 严禁转发信息早报，广告，娱乐短视频，投票等与群主题无关的信息，发现立即清除出群。
-5. 禁止在社群内发布其他社群的二维码（包含文字，二维码等）'''
-            if 'cc' in msg['Text']:
-                robot.send(auto_reply, msg['FromUserName'])
+            '''
+            用户回复数字，发送分群链接；否则，自动回复文案
+            :param msg:
+            :return:
+            '''
+            auto_reply = '''这是由名片全能王筹建的商务合作社群，是人脉共享，资源对接的免费共享平台，名片全能王app有2亿优质商务用户，目前社群人数30,000+，群数量100+。
 
-        @robot.msg_register([PICTURE])
-        def add_group_reply(msg):
-            friend = robot.search_friends(userName=msg['FromUserName'])
-            print('friend----------------------->',friend)
-            chatrooms = robot.search_chatrooms(name='CC微信机器人开发')
-            print('chatrooms-------------------->',chatrooms)
-            robot.add_member_into_chatroom(chatrooms[0]['UserName'], [friend], useInvitation=True)
+【1023】机械机电自动化交流群
+【1024】金融行业交流群
+【1025】IT互联网行业交流群
+【1026】房产建筑行业交流群
+【1027】快消零售交流群
+【1028】广告媒体交流群
+【1029】教育行业交流群
+【1030】医疗行业交流群
+【1031】电子电器行业交流群
+【1032】交通运输物流交流群
+【1033】化工行业交流群
+【1034】冶炼五金交流群
+【1035】能源资源交流群
+
+回复行业群前面对应的4位数字，获取进群邀请。
+
+【入群必读】为了保证行业群群环境良好，进群后会有严格群规，请大家严格遵守。
+
+【备注】如果以上细分群没有您感兴趣的，敬请等待第三期细分群邀请。'''
+
+            if msg['Text'] in FenqunRobot.division_group_map.keys():
+                friend = robot.search_friends(userName=msg['FromUserName'])
+                print('friend----------------------->', friend)
+                chatrooms = robot.search_chatrooms(name=FenqunRobot.division_group_map.get(msg['Text']))
+                print('chatrooms-------------------->', chatrooms)
+                if friend and chatrooms:
+                    robot.add_member_into_chatroom(chatrooms[0]['UserName'], [friend], useInvitation=True)
+            else:
+                robot.send(auto_reply, msg['FromUserName'])
 
         @robot.msg_register(FRIENDS)
         def add_friend(msg):
-            robot.add_friend(**msg['Text'])  # 该操作会自动将新好友的消息录入，不需要重载通讯录
-            opening_remarks = '''您好，我是名片全能王社群的群小蜜，请仔细阅读下方文字哦：
+            '''
+            自动加好友，并回复入群指示
+            :param msg:
+            :return:
+            '''
+            robot.add_friend(**msg['Text'])
+            auto_reply = '''这是由名片全能王筹建的商务合作社群，是人脉共享，资源对接的免费共享平台，名片全能王app有2亿优质商务用户，目前社群人数30,000+，群数量100+。
+
+【1023】机械机电自动化交流群
+【1024】金融行业交流群
+【1025】IT互联网行业交流群
+【1026】房产建筑行业交流群
+【1027】快消零售交流群
+【1028】广告媒体交流群
+【1029】教育行业交流群
+【1030】医疗行业交流群
+【1031】电子电器行业交流群
+【1032】交通运输物流交流群
+【1033】化工行业交流群
+【1034】冶炼五金交流群
+【1035】能源资源交流群
+
+回复行业群前面对应的4位数字，获取进群邀请。
+
+【入群必读】为了保证行业群群环境良好，进群后会有严格群规，请大家严格遵守。
+
+【备注】如果以上细分群没有您感兴趣的，敬请等待第三期细分群邀请。'''
+            robot.send(auto_reply, msg['RecommendInfo']['UserName'])
+
+    def run(self, qr_callback):
+        print('new editor {} login...'.format(self.id))
+        Dir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'editor_data')
+        self.robot.auto_login(hotReload=True, statusStorageDir=os.path.join(Dir, self.id+'.pkl'),
+                              picDir=os.path.join(Dir, self.id+'.png'), qrCallback=qr_callback)
+        self.robot.run(blockThread=False)
+
+
+class ZongqunRobot(EditorRobot):
+    def __init__(self, id, robot):
+        EditorRobot.__init__(self, id, robot)
+
+        @robot.msg_register([PICTURE])
+        def add_group_reply(msg):
+            '''
+            收到用户截图，发送总群链接
+            :param msg:
+            :return:
+            '''
+            friend = robot.search_friends(userName=msg['FromUserName'])
+            print('friend----------------------->', friend)
+            chatrooms = robot.search_chatrooms(name=choice(EditorRobot.center_group))
+            print('chatrooms-------------------->', chatrooms)
+            if friend and chatrooms:
+                robot.add_member_into_chatroom(chatrooms[0]['UserName'], [friend], useInvitation=True)
+
+        @robot.msg_register(FRIENDS)
+        def add_friend(msg):
+            '''
+            自动加好友，并回复自我介绍和入群指示
+            :param msg:
+            :return:
+            '''
+            robot.add_friend(**msg['Text'])
+            auto_reply = '''您好，我是名片全能王社群的群小蜜，请仔细阅读下方文字哦：
+
 【入群必读】
 这是由名片全能王筹建的商务合作社群，是人脉共享，资源对接的共享平台，名片全能王app有2亿优质商务用户，目前社群人数30,000+，群数量100+。
 
 【进群门槛】
-因为名额有限，所以会有相应的限制。麻烦您将下图分享至朋友圈，并截图发回给我，审核通过后，会将您拉进群(注意:请勿分组或秒删)
-
-【入群后福利】
-1、免会费进入【名片全能王】社群，并且第一时间拿到精准客户需求。
-2、将您的商务合作需求群发至100+个商务群，免费推广。
-3、参与【名片全能王】专属活动及专属职场技能培训。
+本社群不收取任何会费，所有服务皆免费，为了壮大我们的社群，有一个小小的请求，希望您将以下海报分享到朋友圈并截图发给我，我才会拉你入群哦！非常感谢您的配合。 
 
 【分享如下海报到朋友圈】'''
-            robot.send(opening_remarks, msg['RecommendInfo']['UserName'])
+            robot.send(auto_reply, msg['RecommendInfo']['UserName'])
             data_dir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'master_data')
-            robot.send('@img@{}'.format(os.path.join(data_dir, 'opening_pic.png')), msg['RecommendInfo']['UserName'])
+            robot.send('@img@{}'.format(os.path.join(data_dir, choice(EditorRobot.editor_pic))), msg['RecommendInfo']['UserName'])
 
-    def run(self, qrCallBack):
+    def run(self, qr_callback):
         print('new editor {} login...'.format(self.id))
         Dir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'editor_data')
-        self.robot.auto_login(hotReload=True, statusStorageDir=os.path.join(Dir, self.id+'.pkl'), picDir=os.path.join(Dir, self.id+'.png'), qrCallback=qrCallBack)
+        self.robot.auto_login(hotReload=True, statusStorageDir=os.path.join(Dir, self.id + '.pkl'),
+                              picDir=os.path.join(Dir, self.id + '.png'), qrCallback=qr_callback)
         self.robot.run(blockThread=False)
--- master_robot.py
+++ master_robot.py
@@ -2,33 +2,48 @@ import os
 import platform
 import itchat
 from itchat.content import *
-from editor_robot import EditorRobot
+from editor_robot import FenqunRobot, ZongqunRobot
 
-qr_max_tried_count = 0
+QR_MAX_TRIED_COUNT = 0
+HELP = '''
+你好！我是机器人管理员。
+回复“总群机器人”——获取总群机器人的身份二维码
+回复“分群机器人”——获取分群机器人的身份二维码
+
+注意事项：
+1）获取二维码之后扫描登录即可成为相应机器人
+2）获取二维码后不登录超过一定时间，二维码会失效，若失效请重新操作
+3）每个二维码只能供一个机器人使用
+4）在微信中退出网页版登录即可退出机器人模式
+'''
 
 
 @itchat.msg_register([TEXT])
 def text_reply(msg):
-    print(msg)
-    global qr_max_tried_count
-    qr_max_tried_count = 0
+    global QR_MAX_TRIED_COUNT
+    QR_MAX_TRIED_COUNT = 0
 
-    def editorQrCallBack(uuid, status, qrcode):
-        global qr_max_tried_count
-        if qr_max_tried_count<2:
+    def add_editor_qr_callback(uuid, status, qrcode):
+        global QR_MAX_TRIED_COUNT
+        if QR_MAX_TRIED_COUNT<2:
             pic_file = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'editor_data', msg['FromUserName'] + '.png')
             with open(pic_file, 'wb') as f:
                 f.write(qrcode)
-            ret = itchat.send('Please scan the QR code to log in.', msg['FromUserName'])
-            ret = itchat.send('@img@{}'.format(pic_file), msg['FromUserName'])
-        elif qr_max_tried_count>5 or str(status)=='408':
-            ret = itchat.send('Scan timeout, Please request again', msg['FromUserName'])
+            itchat.send('Please scan the QR code to log in.', msg['FromUserName'])
+            itchat.send('@img@{}'.format(pic_file), msg['FromUserName'])
+        elif QR_MAX_TRIED_COUNT>5 or str(status)=='408':
+            itchat.send('Scan timeout, Please request again', msg['FromUserName'])
             exit(0)
-        qr_max_tried_count += 1
+        QR_MAX_TRIED_COUNT += 1
 
-    if 'master' in msg['Text']:
-        editor = EditorRobot(id=msg['FromUserName'], robot=itchat.new_instance())
-        editor.run(editorQrCallBack)
+    if '总群机器人' in msg['Text']:
+        editor = ZongqunRobot(id=msg['FromUserName'], robot=itchat.new_instance())
+        editor.run(add_editor_qr_callback)
+    elif '分群机器人' in msg['Text']:
+        editor = FenqunRobot(id=msg['FromUserName'], robot=itchat.new_instance())
+        editor.run(add_editor_qr_callback)
+    else:
+        itchat.send(HELP, msg['FromUserName'])
 
 master_data_dir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'master_data')
 if 'Linux' in platform.platform():
